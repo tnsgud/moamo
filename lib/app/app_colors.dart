f0f9832f260d9e260566734eb845227e69135930
tnsgud <qkrtnsgud0229@gmail.com>
2026-09-20 18:59:25 +0900

    feat: color system 추가

---
+++ lib/app/app_colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppColors {
+  // Brand
+  static const primary = Color(0xFFFFD467);
+  static const secondary100 = Color(0xFF213448);
+  static const profile = Color(0xFFEDF1D0);
+  static const primary80 = Color.fromRGBO(255, 212, 103, 0.8);
+
+  // Neutral
+  static const neutral = Color(0xFF121212);
+  static const neutral90 = Color(0xFF1A1A1A);
+  static const neutral70 = Color(0xFF3D3D3D);
+  static const neutral60 = Color(0xFF4B4B4B);
+  static const neutral50 = Color(0xFF8B8B8B);
+  static const neutral40 = Color(0xFFBCBCBC);
+  static const neutral20 = Color(0xFFDEDEDE);
+  static const neutral15 = Color(0xFFEDEDED);
+  static const neutral10 = Color(0xFFF3F3F3);
+  static const neutral05 = Color(0xFFF7F7F7);
+  static const neutral0 = Color(0xFFFFFFFF);
+
+  // Status
+  static const notification = Color(0xFFF63939);
+}
