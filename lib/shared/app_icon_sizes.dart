b16c83da06fec7d422678e6f34808452f3c78a69
tnsgud <qkrtnsgud0229@gmail.com>
2026-09-23 23:53:41 +0900

    feat: icon size 추가

---
+++ lib/shared/app_icon_sizes.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppIconSizes {
+  static const xs = Size(16, 16);
+  static const sm = Size(20, 20);
+  static const md = Size(24, 24);
+  static const lg = Size(28, 28);
+  static const xl = Size(32, 32);
+  static const xxl = Size(48, 48);
+  static const xxxl = Size(64, 64);
+}
