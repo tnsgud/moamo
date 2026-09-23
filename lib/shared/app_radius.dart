d485a8292bca59053673fc4a837f845bb7fdf2c4
tnsgud <qkrtnsgud0229@gmail.com>
2026-09-23 23:57:07 +0900

    feat: radius 추가

---
+++ lib/shared/app_radius.dart
@@ -0,0 +1,8 @@
+abstract final class AppRadius {
+  static const xs = 8;
+  static const sm = 12;
+  static const md = 16;
+  static const lg = 20;
+  static const xl = 24;
+  static const xxl = 32;
+}
