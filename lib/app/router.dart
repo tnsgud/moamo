e943bb978c9a775d181a9ab1ef048d8680e23d4d
tnsgud <qkrtnsgud0229@gmail.com>
2026-09-24 00:23:11 +0900

    feat: splash화면 임시 추가

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -2,5 +2,6 @@ import 'package:go_router/go_router.dart';
 import 'package:moamo/features/auth/ui/login_screen.dart';
 
 final appRouter = GoRouter(
+  initialLocation: '/login',
   routes: [GoRoute(path: '/login', builder: (context, state) => LoginScreen())],
 );
