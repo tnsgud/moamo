4e9bdb5b562274a7bc88d693c385ea042bdf1052
tnsgud <qkrtnsgud0229@gmail.com>
2026-09-23 23:56:44 +0900

    fix: icon size Size class -> double

--- lib/shared/app_icon_sizes.dart
+++ lib/shared/app_icon_sizes.dart
@@ -1,11 +1,9 @@
-import 'package:flutter/material.dart';
-
 abstract final class AppIconSizes {
-  static const xs = Size(16, 16);
-  static const sm = Size(20, 20);
-  static const md = Size(24, 24);
-  static const lg = Size(28, 28);
-  static const xl = Size(32, 32);
-  static const xxl = Size(48, 48);
-  static const xxxl = Size(64, 64);
+  static const xs = 16;
+  static const sm = 20;
+  static const md = 24;
+  static const lg = 28;
+  static const xl = 32;
+  static const xxl = 48;
+  static const xxxl = 64;
 }
